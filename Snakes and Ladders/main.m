85aeb0172079dd530b30b3b3bc1a312c81cfe0dd
Colin Russell <colinrussell101@gmail.com>
2018-04-15 11:36:00 -0400

    Initial Commit

---
+++ Snakes and Ladders/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Snakes and Ladders
+//
+//  Created by Colin on 2018-04-15.
+//  Copyright © 2018 Colin Russell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
